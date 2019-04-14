692bde5f0284eafce7022b980319ad999a80ffe0
Ilya Degtyarenko <is.degtyarenko@gmail.com>
2019-04-14 10:19:13 +0300

    Vue component parts.

--- resources/js/components/content/CurrentSurvey.vue
+++ resources/js/components/content/CurrentSurvey.vue
@@ -1,6 +1,22 @@
+<template>
+    <section>
+        Survey with id: {{$route.params.id}}
+        <small>{{$route.params}}</small>
 
 
         <h1>Привет, меня зовут Илья!</h1>
         <p>
             Привет еще раз! Значит так! Слушай меня внимательно, суперсила это секс, ОМска, Дубаи, золотой состав нашщей группировки. Это конгламерат, по верхам, лям стоит, голову спарта троя!
         </p>
+    </section>
+</template>
+
+<script>
+    export default {
+        name: 'Current'
+    }
+</script>
+
+<style scoped>
+
+</style>
